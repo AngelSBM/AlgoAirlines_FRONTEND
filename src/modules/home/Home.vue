b18773fab946d0cbf2b3ee559b6004f4cbc17135
Angel Bello <angelsebastianbellomateo@gmail.com>
2022-10-16 17:30:31 -0400

    Limpiando proyecto

---
+++ src/modules/home/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  
+  <div class="">
+    HOLAAAAA
+  </div>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Home from '@/modules/home/Home'
 
 Vue.use(VueRouter)
 
@@ -8,18 +8,10 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+      return import(/* webpackChunkName: "home" */ '@/modules/home/Home')
     }
-  }
+  },
 ]
 
 const router = new VueRouter({
